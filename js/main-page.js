eef314a756fcaede418e9833e46d0cc93a403bed
hAudeSecond <hauvtct@gmail.com>
2023-07-19 20:15:29 +0700

    update search bar

--- js/main-page.js
+++ js/main-page.js
@@ -13,17 +13,43 @@ function timkiem() {
 
     for (let i = 0; i < v1.length; i++) {
         var h1 = v1[i].textContent;
-        if (h1.indexOf(k1) >= 0) {
-            v1[i].setAttribute("style", "color:red;");
+        h1 = h1.toUpperCase();
+        if (h1.indexOf(k1.toUpperCase()) >= 0) {
+            v1[i].setAttribute("style", "color:#CC6699;");
+        }
+    }
+}
+function timkiemmain() {
+    let k3 = document.getElementById("search").value;
+    let v3 = document.querySelectorAll(".main-info");
+
+    for (let i = 0; i < v3.length; i++) {
+        var h3 = v3[i].textContent;
+        h3 = h3.toUpperCase();
+        if (h3.indexOf(k3.toUpperCase()) >= 0) {
+            v3[i].setAttribute("style", "color:#CC6699;");
+        }
+    }
+}
+function recolormain() {
+    let k4 = document.getElementById("search").value;
+    let v4 = document.querySelectorAll(".main-info")
+    for (let i = 0; i < v4.length; i++) {
+        var h4 = v4[i].textContent;
+        h4 = h4.toUpperCase();
+        if (h4.indexOf(k4.toUpperCase()) >= 0) {
+            v4[i].setAttribute("style", "color:darkolivegreen;");
         }
     }
 }
 function recolor() {
-    let
-    for (let i = 0; i < v.length; i++) {
-        var h = v[i].textContent;
-        if (h.indexOf(k) >= 0) {
-            v[i].setAttribute("style", "color:white;");
+    let k2 = document.getElementById("search").value;
+    let v2 = document.querySelectorAll(".admission-container .type")
+    for (let i = 0; i < v2.length; i++) {
+        var h2 = v2[i].textContent;
+        h2 = h2.toUpperCase();
+        if (h2.indexOf(k2.toUpperCase()) >= 0) {
+            v2[i].setAttribute("style", "color:white;");
         }
     }
 }
