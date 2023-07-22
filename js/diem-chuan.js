043ffe4fae6b586baf9ea3656fbf46b93941f616
hAudeSecond <hauvtct@gmail.com>
2023-07-22 12:05:38 +0700

    add-tin tuyen sinh

--- js/diem-chuan.js
+++ js/diem-chuan.js
@@ -6,3 +6,26 @@ setInterval(function () {
         counter = 1;
     }
 }, 6000);
+
+function timdiemchuan() {
+    let k1 = document.getElementById("search").value;
+    let v1 = document.getElementsByTagName("td");
+    for (let i = 0; i < v1.length; i++) {
+        var h1 = v1[i].textContent;
+        h1 = h1.toUpperCase();
+        if (h1.indexOf(k1.toUpperCase()) >= 0) {
+            v1[i].setAttribute("style", "color:#CC6699; font-weight:bold;");
+        }
+    }
+}
+function recolordiemchuan() {
+    let k2 = document.getElementById("search").value;
+    let v2 = document.getElementsByTagName("td");
+    for (let i = 0; i < v2.length; i++) {
+        var h2 = v2[i].textContent;
+        h2 = h2.toUpperCase();
+        if (h2.indexOf(k2.toUpperCase()) >= 0) {
+            v2[i].setAttribute("style", "color:#004B40; font-weight:normal;");
+        }
+    }
+}
